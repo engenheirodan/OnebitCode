8f9657d571b8244ce31def7d7346e8a43430ba91
Danilo Santos de Araujo <engenheirodan@outlook.com>
2022-12-16 07:12:31 -0300

    Alterando status

---
+++ A.primeiro passos/4.recursos-nativos.js
@@ -0,0 +1,11 @@
+//comandos basicos do console
+
+console.log("Bem vindo á nave elemental");
+console.info("retorna mensagem com tonalidade diferente")
+console.warn("mensagem de urngencias")
+console.error("mostrar alguma mensagem de erro do browser")
+
+alert("usado para exibir uma mensagem na tela");
+confirm("Nave ligada");
+
+prompt("exibi mensagem com input")
